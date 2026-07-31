3810f2e8a7593bdaedf07b263886f68c02db9f49
tevas007 <109091323+tevas007@users.noreply.github.com>
2026-07-31 03:34:58 +0300

    Create m53.js

---
+++ routes/m53.js
@@ -0,0 +1,25 @@
+registerRoute({
+  id: "M_Nr53",
+  name: "Maršrutas Nr. 53",
+  title: "Ignalina – N. Daugėliškis",
+  subtitle: "Ignalina – N. Daugėliškis (Mokslo metais, I–V)",
+  distance: "12,5 km",
+  trip1_name: "Iš Ignalinos (07:30)",
+  trip2_name: "Iš N. Daugėliškio (07:55)",
+  trip1_stops: [
+    { name: "Ignalina", time: "07:30", dist: "0 km" },
+    { name: "Baltys", time: "07:35", dist: "3,2 km" },
+    { name: "Vidiškės", time: "07:40", dist: "5,2 km" },
+    { name: "Maksimonys", time: "07:43", dist: "8,0 km" },
+    { name: "Girininkija", time: "07:45", dist: "11,0 km" },
+    { name: "N.Daugėliškis", time: "07:50", dist: "12,5 km" }
+  ],
+  trip2_stops: [
+    { name: "N.Daugėliškis", time: "07:55", dist: "0 km" },
+    { name: "Girininkija", time: "07:57", dist: "1,5 km" },
+    { name: "Maksimonys", time: "08:00", dist: "4,5 km" },
+    { name: "Vidiškės", time: "08:05", dist: "7,3 km" },
+    { name: "Baltys", time: "08:10", dist: "9,3 km" },
+    { name: "Ignalina", time: "08:15", dist: "12,5 km" }
+  ]
+});
