60eb793981df698a51eca6ad310a9f27fd75c69b
tevas007 <109091323+tevas007@users.noreply.github.com>
2026-07-31 03:38:59 +0300

    Create m57-00.js

---
+++ routes/m57-00.js
@@ -0,0 +1,39 @@
+registerRoute({
+  id: "M_Nr57-00",
+  name: "Maršrutas Nr. 57-00",
+  title: "Ignalina – Mažėnai",
+  subtitle: "Ignalina – Mažėnai (Kasdien)",
+  distance: "25,4 km",
+  trip1_name: "Iš Ignalinos (17:20)",
+  trip2_name: "Iš Mažėnų (18:10)",
+  trip1_stops: [
+    { name: "Ignalina", time: "17:20", dist: "0 km" },
+    { name: "Baltys", time: "17:25", dist: "3,2 km" },
+    { name: "Vidiškės", time: "17:29", dist: "5,2 km" },
+    { name: "Maksimonys", time: "17:34", dist: "8,0 km" },
+    { name: "Girininkija", time: "17:39", dist: "11,0 km" },
+    { name: "N.Daugėliškis", time: "17:42", dist: "12,5 km" },
+    { name: "Kampelis", time: "17:47", dist: "15,1 km" },
+    { name: "Taujūnai", time: "17:50", dist: "16,2 km" },
+    { name: "Ožionių kryžk.", time: "17:54", dist: "18,4 km" },
+    { name: "Janionys", time: "17:56", dist: "19,6 km" },
+    { name: "Padysnis", time: "18:00", dist: "21,7 km" },
+    { name: "Pamunė", time: "18:03", dist: "23,0 km" },
+    { name: "Mažėnai", time: "18:08", dist: "25,4 km" }
+  ],
+  trip2_stops: [
+    { name: "Mažėnai", time: "18:10", dist: "0 km" },
+    { name: "Pamunė", time: "18:14", dist: "2,4 km" },
+    { name: "Padysnis", time: "18:16", dist: "3,7 km" },
+    { name: "Janionys", time: "18:20", dist: "5,8 km" },
+    { name: "Ožionių kryžk.", time: "18:25", dist: "7,0 km" },
+    { name: "Taujūnai", time: "18:29", dist: "9,2 km" },
+    { name: "Kampelis", time: "18:32", dist: "10,3 km" },
+    { name: "N.Daugėliškis", time: "18:36", dist: "12,9 km" },
+    { name: "Girininkija", time: "18:38", dist: "14,4 km" },
+    { name: "Maksimonys", time: "18:42", dist: "17,4 km" },
+    { name: "Vidiškės", time: "18:46", dist: "20,2 km" },
+    { name: "Baltys", time: "18:48", dist: "22,2 km" },
+    { name: "Ignalina", time: "18:50", dist: "25,4 km" }
+  ]
+});
