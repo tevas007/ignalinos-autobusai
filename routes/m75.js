4d6e8434b0d961b46e5baedb5c923e76c9c8839e
tevas007 <109091323+tevas007@users.noreply.github.com>
2026-07-31 04:11:27 +0300

    Create m75.js

---
+++ routes/m75.js
@@ -0,0 +1,39 @@
+registerRoute({
+  id: "m75",
+  name: "Maršrutas Nr. 75",
+  title: "Ignalina – Kirdeikiai",
+  subtitle: "Reisų vykdymo dienos: I, V",
+  distance: "23.5 km",
+  trip1_name: "5:55 iš Ignalinos",
+  trip2_name: "6:45 iš Kirdeikių",
+  trip1_stops: [
+    { time: "05:55", name: "Ignalina", dist: "0 km" },
+    { time: "05:57", name: "Ligoninė", dist: "0.8 km" },
+    { time: "06:00", name: "Strigailiškis", dist: "2.1 km" },
+    { time: "06:02", name: "Gaveikėnai", dist: "3.3 km" },
+    { time: "06:04", name: "Jonalaukis", dist: "5.1 km" },
+    { time: "06:07", name: "Dringis", dist: "7.1 km" },
+    { time: "06:11", name: "Vaišniūnai", dist: "9.6 km" },
+    { time: "06:17", name: "Trainiškis", dist: "13.0 km" },
+    { time: "06:19", name: "Vaidziuškės", dist: "14.6 km" },
+    { time: "06:23", name: "Ginučiai", dist: "16.7 km" },
+    { time: "06:27", name: "Ginučių mokykla", dist: "18.0 km" },
+    { time: "06:30", name: "Paukoja", dist: "20.5 km" },
+    { time: "06:35", name: "Kirdeikiai", dist: "23.5 km" }
+  ],
+  trip2_stops: [
+    { time: "06:45", name: "Kirdeikiai", dist: "0 km" },
+    { time: "06:49", name: "Paukoja", dist: "3.0 km" },
+    { time: "06:52", name: "Ginučių mokykla", dist: "5.5 km" },
+    { time: "06:54", name: "Ginučiai", dist: "6.8 km" },
+    { time: "06:58", name: "Vaidziuškės", dist: "8.9 km" },
+    { time: "07:01", name: "Trainiškis", dist: "10.5 km" },
+    { time: "07:08", name: "Vaišniūnai", dist: "13.9 km" },
+    { time: "07:12", name: "Dringis", dist: "16.4 km" },
+    { time: "07:15", name: "Jonalaukis", dist: "18.4 km" },
+    { time: "07:18", name: "Gaveikėnai", dist: "20.2 km" },
+    { time: "07:22", name: "Strigailiškis", dist: "21.4 km" },
+    { time: "07:23", name: "Ligoninė", dist: "22.7 km" },
+    { time: "07:25", name: "Ignalina", dist: "23.5 km" }
+  ]
+});
