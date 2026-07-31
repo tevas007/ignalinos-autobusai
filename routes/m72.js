4c3c77f9a9f9d7ba6152f1f3ca69792ae070fead
tevas007 <109091323+tevas007@users.noreply.github.com>
2026-07-31 04:07:25 +0300

    Create m72.js

---
+++ routes/m72.js
@@ -0,0 +1,33 @@
+registerRoute({
+  id: "m72",
+  name: "Maršrutas Nr. 72",
+  title: "Ignalina – Ginučiai",
+  subtitle: "Reisų vykdymo dienos: I, V",
+  distance: "17.7 km",
+  trip1_name: "13:50 iš Ignalinos",
+  trip2_name: "14:22 iš Ginučių",
+  trip1_stops: [
+    { time: "13:50", name: "Ignalina", dist: "0 km" },
+    { time: "13:52", name: "Ligoninė", dist: "0.8 km" },
+    { time: "13:54", name: "Strigailiškis", dist: "2.1 km" },
+    { time: "13:56", name: "Gaveikėnai", dist: "3.3 km" },
+    { time: "13:58", name: "Jonalaukis", dist: "5.1 km" },
+    { time: "14:02", name: "Dringis", dist: "7.1 km" },
+    { time: "14:06", name: "Vaišniūnai", dist: "9.6 km" },
+    { time: "14:13", name: "Trainiškis", dist: "13.0 km" },
+    { time: "14:16", name: "Vaidziuškės", dist: "14.6 km" },
+    { time: "14:19", name: "Ginučiai", dist: "17.7 km" }
+  ],
+  trip2_stops: [
+    { time: "14:22", name: "Ginučiai", dist: "0 km" },
+    { time: "14:25", name: "Vaidziuškės", dist: "3.1 km" },
+    { time: "14:28", name: "Trainiškis", dist: "4.7 km" },
+    { time: "14:34", name: "Vaišniūnai", dist: "8.1 km" },
+    { time: "14:38", name: "Dringis", dist: "10.6 km" },
+    { time: "14:42", name: "Jonalaukis", dist: "12.6 km" },
+    { time: "14:44", name: "Gaveikėnai", dist: "14.4 km" },
+    { time: "14:46", name: "Strigailiškis", dist: "15.6 km" },
+    { time: "14:48", name: "Ligoninė", dist: "16.9 km" },
+    { time: "14:50", name: "Ignalina", dist: "17.7 km" }
+  ]
+});
