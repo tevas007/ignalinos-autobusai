b91a567765f318d25c3cfaf1a8037a3f900906e1
tevas007 <109091323+tevas007@users.noreply.github.com>
2026-07-31 04:16:14 +0300

    Create m81.js

---
+++ routes/m81.js
@@ -0,0 +1,27 @@
+registerRoute({
+  id: "m81",
+  name: "Maršrutas Nr. 81",
+  title: "Ignalina – Dūdos",
+  subtitle: "Reisų vykdymo dienos: Pirmadienis, Antradienis, Trečiadienis, Ketvirtadienis, Penktadienis",
+  distance: "12.1 km",
+  trip1_name: "5:30 / 15:30 iš Ignalinos",
+  trip2_name: "5:55 / 6:20 iš Dūdų",
+  trip1_stops: [
+    { time: "05:30", name: "Ignalina", dist: "0 km" },
+    { time: "05:34", name: "Baltys", dist: "3.2 km" },
+    { time: "05:38", name: "Vidiškės", dist: "5.2 km" },
+    { time: "05:42", name: "Rimšėnai", dist: "7.0 km" },
+    { time: "05:47", name: "Kaneišiai", dist: "9.6 km" },
+    { time: "05:51", name: "Kukoriškė", dist: "11.1 km" },
+    { time: "05:53", name: "Dūdos", dist: "12.1 km" }
+  ],
+  trip2_stops: [
+    { time: "05:55", name: "Dūdos", dist: "0 km" },
+    { time: "05:57", name: "Kukoriškė", dist: "1.0 km" },
+    { time: "06:01", name: "Kaneišiai", dist: "2.5 km" },
+    { time: "06:06", name: "Rimšėnai", dist: "5.1 km" },
+    { time: "06:10", name: "Vidiškės", dist: "6.9 km" },
+    { time: "06:14", name: "Baltys", dist: "8.9 km" },
+    { time: "06:20", name: "Ignalina", dist: "12.1 km" }
+  ]
+});
