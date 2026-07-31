dcef7ef627e3912bd055bc6f7d5885db81ed3987
tevas007 <109091323+tevas007@users.noreply.github.com>
2026-07-31 04:16:42 +0300

    Create m82.js

---
+++ routes/m82.js
@@ -0,0 +1,39 @@
+registerRoute({
+  id: "m82",
+  name: "Maršrutas Nr. 82",
+  title: "Ignalina – Rubelninkai",
+  subtitle: "Reisų vykdymo dienos: Pirmadienis, Antradienis, Trečiadienis, Ketvirtadienis, Penktadienis",
+  distance: "25.1 km",
+  trip1_name: "14:00 iš Ignalinos",
+  trip2_name: "15:25 iš Rubelninkų",
+  trip1_stops: [
+    { time: "14:00", name: "Ignalina", dist: "0 km" },
+    { time: "14:04", name: "Baltys", dist: "3.2 km" },
+    { time: "14:09", name: "Vidiškės", dist: "5.2 km" },
+    { time: "14:13", name: "Maksimonys", dist: "8.0 km" },
+    { time: "14:18", name: "Girininkija", dist: "11.0 km" },
+    { time: "14:22", name: "N.Daugėliškis", dist: "12.5 km" },
+    { time: "14:27", name: "Mikalavas I", dist: "15.4 km" },
+    { time: "14:29", name: "Mikalavas II", dist: "16.5 km" },
+    { time: "14:33", name: "Grigiškė", dist: "18.3 km" },
+    { time: "14:35", name: "Paringys", dist: "19.5 km" },
+    { time: "14:40", name: "Balciulioniškė", dist: "22.0 km" },
+    { time: "14:43", name: "Pustoška", dist: "23.5 km" },
+    { time: "14:45", name: "Rubelninkai", dist: "25.1 km" }
+  ],
+  trip2_stops: [
+    { time: "15:25", name: "Rubelninkai", dist: "0 km" },
+    { time: "15:22", name: "Pustoška", dist: "1.6 km" },
+    { time: "15:18", name: "Balciulioniškė", dist: "3.1 km" },
+    { time: "15:15", name: "Paringys", dist: "5.6 km" },
+    { time: "15:12", name: "Grigiškė", dist: "6.8 km" },
+    { time: "15:09", name: "Mikalavas II", dist: "8.6 km" },
+    { time: "15:04", name: "Mikalavas I", dist: "9.7 km" },
+    { time: "15:03", name: "N.Daugėliškis", dist: "12.6 km" },
+    { time: "14:59", name: "Girininkija", dist: "14.1 km" },
+    { time: "14:57", name: "Maksimonys", dist: "17.1 km" },
+    { time: "14:52", name: "Vidiškės", dist: "19.9 km" },
+    { time: "14:49", name: "Baltys", dist: "21.9 km" },
+    { time: "14:47", name: "Ignalina", dist: "25.1 km" }
+  ]
+});
