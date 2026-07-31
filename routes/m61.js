ea87a99416b7eb48573f320e37f6b4cd554a37d1
tevas007 <109091323+tevas007@users.noreply.github.com>
2026-07-31 03:35:47 +0300

    Create m61.js

---
+++ routes/m61.js
@@ -0,0 +1,29 @@
+registerRoute({
+  id: "M_Nr61",
+  name: "Maršrutas Nr. 61",
+  title: "Ignalina – Meironys",
+  subtitle: "Ignalina – Meironys (Trečiadieniais)",
+  distance: "8,3 km",
+  trip1_name: "Iš Ignalinos (08:10 / 12:00)",
+  trip2_name: "Iš Meironių (08:30 / 12:20)",
+  trip1_stops: [
+    { name: "Ignalina", time: "08:10 / 12:00", dist: "0 km" },
+    { name: "Ligoninė", time: "08:12 / 12:02", dist: "0,9 km" },
+    { name: "Akligatvis", time: "08:14 / 12:04", dist: "1,7 km" },
+    { name: "Strigailiškis", time: "08:16 / 12:06", dist: "2,6 km" },
+    { name: "Gaveikėnai", time: "08:21 / 12:11", dist: "4,7 km" },
+    { name: "Dringykštis", time: "08:24 / 12:14", dist: "5,9 km" },
+    { name: "Meironys I", time: "08:26 / 12:16", dist: "7,1 km" },
+    { name: "Meironys II", time: "08:28 / 12:18", dist: "8,3 km" }
+  ],
+  trip2_stops: [
+    { name: "Meironys II", time: "08:30 / 12:20", dist: "0 km" },
+    { name: "Meironys I", time: "08:33 / 12:23", dist: "1,2 km" },
+    { name: "Dringykštis", time: "08:35 / 12:25", dist: "2,4 km" },
+    { name: "Gaveikėnai", time: "08:38 / 12:28", dist: "3,6 km" },
+    { name: "Strigailiškis", time: "08:43 / 12:33", dist: "5,7 km" },
+    { name: "Akligatvis", time: "08:45 / 12:35", dist: "6,6 km" },
+    { name: "Ligoninė", time: "08:47 / 12:37", dist: "7,4 km" },
+    { name: "Ignalina", time: "08:50 / 12:40", dist: "8,3 km" }
+  ]
+});
