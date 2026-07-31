55ddc0c724ec9a51c135caad18fcc9e4fabbee10
tevas007 <109091323+tevas007@users.noreply.github.com>
2026-07-31 04:06:57 +0300

    Create m71.js

---
+++ routes/m71.js
@@ -0,0 +1,33 @@
+registerRoute({
+  id: "m71",
+  name: "Maršrutas Nr. 71",
+  title: "Ignalina – Antalksnė",
+  subtitle: "Reisų vykdymo dienos: I, II, III, IV, V",
+  distance: "20.5 km",
+  trip1_name: "Iš Ignalinos (6:30 / 14:45 / 17:25)",
+  trip2_name: "Iš Antalksnės (7:10 / 15:25 / 18:06)",
+  trip1_stops: [
+    { time: "06:30 / 14:45 / 17:25", name: "Ignalina", dist: "0 km" },
+    { time: "06:32 / 14:47 / 17:27", name: "Ligoninė", dist: "0.8 km" },
+    { time: "06:38 / 14:52 / 17:33", name: "Palūšė", dist: "4.7 km" },
+    { time: "06:43 / 14:57 / 17:39", name: "Šakarva", dist: "8.8 km" },
+    { time: "06:45 / 15:00 / 17:41", name: "Motiejūnai", dist: "9.7 km" },
+    { time: "06:48 / 15:02 / 17:44", name: "Grikiapelė", dist: "11.1 km" },
+    { time: "06:50 / 15:04 / 17:46", name: "Pupiškė", dist: "12.1 km" },
+    { time: "06:57 / 15:11 / 17:53", name: "Linkmenys", dist: "15.9 km" },
+    { time: "07:01 / 15:15 / 17:57", name: "Mozūriškė", dist: "17.7 km" },
+    { time: "07:05 / 15:20 / 18:02", name: "Antalksnė", dist: "20.5 km" }
+  ],
+  trip2_stops: [
+    { time: "07:10 / 15:25 / 18:06", name: "Antalksnė", dist: "0 km" },
+    { time: "07:14 / 15:30 / 18:10", name: "Mozūriškė", dist: "2.8 km" },
+    { time: "07:18 / 15:34 / 18:14", name: "Linkmenys", dist: "4.6 km" },
+    { time: "07:25 / 15:42 / 18:21", name: "Pupiškė", dist: "8.4 km" },
+    { time: "07:27 / 15:44 / 18:23", name: "Grikiapelė", dist: "9.4 km" },
+    { time: "07:30 / 15:47 / 18:26", name: "Motiejūnai", dist: "10.8 km" },
+    { time: "07:32 / 15:49 / 18:28", name: "Šakarva", dist: "11.7 km" },
+    { time: "07:37 / 15:56 / 18:32", name: "Palūšė", dist: "15.8 km" },
+    { time: "07:43 / 16:03 / 18:39", name: "Ligoninė", dist: "19.7 km" },
+    { time: "07:45 / 16:05 / 18:41", name: "Ignalina", dist: "20.5 km" }
+  ]
+});
