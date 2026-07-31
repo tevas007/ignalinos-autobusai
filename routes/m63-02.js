f06cdc6cc6374b3823a957c34e71fdab469beace
tevas007 <109091323+tevas007@users.noreply.github.com>
2026-07-31 03:43:30 +0300

    Create m63-02.js

---
+++ routes/m63-02.js
@@ -0,0 +1,39 @@
+registerRoute({
+  id: "M_Nr63-02",
+  name: "Maršrutas Nr. 63.02",
+  title: "Ignalina – Švedriškė per Serapiniškę",
+  subtitle: "Ignalina – Švedriškė per Serapiniškę (I, V)",
+  distance: "32,6 km",
+  trip1_name: "Iš Jakėnų (06:57)",
+  trip2_name: "Iš Ignalinos (14:50)",
+  trip1_stops: [
+    { name: "Jakėnai", time: "06:57", dist: "0 km" },
+    { name: "Švedriškė", time: "07:00", dist: "2,6 km" },
+    { name: "Ažvinčiai", time: "07:05", dist: "4,6 km" },
+    { name: "Trakai", time: "07:09", dist: "7,5 km" },
+    { name: "Nikiškė", time: "07:14", dist: "9,5 km" },
+    { name: "Serapiniškė", time: "07:20", dist: "11,3 km" },
+    { name: "Nikiškė", time: "07:24", dist: "13,1 km" },
+    { name: "Akriemiai", time: "07:31", dist: "16,9 km" },
+    { name: "Kazitiškis", time: "07:37", dist: "20,3 km" },
+    { name: "Pašvoginė", time: "07:42", dist: "23,5 km" },
+    { name: "Oaza", time: "07:46", dist: "25,8 km" },
+    { name: "Kilatrakis", time: "07:49", dist: "27,6 km" },
+    { name: "Ignalina", time: "07:55", dist: "32,6 km" }
+  ],
+  trip2_stops: [
+    { name: "Ignalina", time: "14:50", dist: "0 km" },
+    { name: "Kilatrakis", time: "14:55", dist: "5,0 km" },
+    { name: "Oaza", time: "14:58", dist: "6,8 km" },
+    { name: "Pašvoginė", time: "15:02", dist: "9,1 km" },
+    { name: "Kazitiškis", time: "15:06", dist: "12,3 km" },
+    { name: "Akriemiai", time: "15:10", dist: "15,7 km" },
+    { name: "Nikiškė", time: "15:16", dist: "19,5 km" },
+    { name: "Serapiniškė", time: "15:20", dist: "21,3 km" },
+    { name: "Nikiškė", time: "15:23", dist: "23,1 km" },
+    { name: "Trakai", time: "15:27", dist: "25,1 km" },
+    { name: "Ažvinčiai", time: "15:30", dist: "28,0 km" },
+    { name: "Švedriškė", time: "15:34", dist: "30,0 km" },
+    { name: "Jakėnai", time: "15:37", dist: "32,6 km" }
+  ]
+});
